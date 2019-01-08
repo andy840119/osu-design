9a3e3aae13d0c405e76f33d970f3d63e1c8f3174
phosphene47 <phosphene47@gmail.com>
2019-01-08 18:07:38 +0900

    Add double converter

--- osu.Framework.Design/Markup/Converters/NumericConverters.cs
+++ osu.Framework.Design/Markup/Converters/NumericConverters.cs
@@ -14,4 +14,15 @@ namespace osu.Framework.Design.Markup.Converters
         public void SerializeAsElement(object value, XElement element) => element.Value = value.ToString();
         public void SerializeAsString(object value, out string data) => data = value.ToString();
     }
+    public class DoubleConverter : IConverter
+    {
+        public Type ConvertingType => typeof(double);
+        public bool PreferStringSerialization => true;
+
+        public object DeserializeFromElement(XElement element, Type type) => double.Parse(element.Value);
+        public object DeserializeFromString(string data, Type type) => double.Parse(data);
+
+        public void SerializeAsElement(object value, XElement element) => element.Value = value.ToString();
+        public void SerializeAsString(object value, out string data) => data = value.ToString();
+    }
 }
