7eefc08ead0a328fd97812b9197277f548630ebc
phosphene47 <phosphene47@gmail.com>
2019-01-08 23:19:49 +0900

    Return whitespace normalized generated node

--- osu.Framework.Design/CodeGeneration/CodeGenerator.cs
+++ osu.Framework.Design/CodeGeneration/CodeGenerator.cs
@@ -23,7 +23,7 @@ namespace osu.Framework.Design.CodeGeneration
             var drawableNode = DrawableGenerator.Generate(drawable, generator);
 
             // Return the root node (namespace) with drawable declaration
-            return generator.NamespaceDeclaration(string.Join('.', _doc.Folders), drawableNode);
+            return generator.NamespaceDeclaration(string.Join('.', _doc.Folders), drawableNode).NormalizeWhitespace();
         }
 
         public void Generate(DrawableData drawable, TextWriter writer) => Generate(drawable).WriteTo(writer);
