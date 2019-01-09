c7831c04a62e319cd94d0ecdb1de493759e4955d
phosphene47 <phosphene47@gmail.com>
2019-01-09 16:34:11 +0900

    Add folder icons

---
+++ osu.Framework.Design/Resources/Textures/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script was used to automatically convert vscode-icons SVG files to PNG using rsvg-convert.
+
+for filename in ./*.svg; do
+    for ((i=0; i<=3; i++)); do
+        rsvg-convert -h 128 "$filename.svg" > "$filename.png"
+    done
+done
