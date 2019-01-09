65b59ac73a299c9ff8b0b76bc014b22e643058a9
phosphene47 <phosphene47@gmail.com>
2019-01-09 16:45:40 +0900

    Add icons to workspace items

--- osu.Framework.Design/Designer/SolutionBrowser.cs
+++ osu.Framework.Design/Designer/SolutionBrowser.cs
@@ -8,8 +8,10 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
 using osu.Framework.Input.Events;
 using osu.Framework.MathUtils;
+using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Framework.Design.Designer
@@ -81,6 +83,7 @@ namespace osu.Framework.Design.Designer
         {
             readonly Container<Item> _content;
             readonly Drawable _head;
+            readonly Sprite _icon;
 
             readonly Drawable _hover;
             readonly Drawable _highlight;
@@ -116,7 +119,7 @@ namespace osu.Framework.Design.Designer
                     {
                         RelativeSizeAxes = Axes.Y,
                         Width = 2,
-                        Colour = folderColours[level % folderColours.Length],
+                        Colour = folderColours[level % folderColours.Length].Opacity(0.5f),
                         Alpha = 0
                     },
                     new FillFlowContainer
@@ -161,9 +164,15 @@ namespace osu.Framework.Design.Designer
                                         RelativeSizeAxes = Axes.X,
                                         AutoSizeAxes = Axes.Y,
                                         Direction = FillDirection.Horizontal,
-                                        Padding = new MarginPadding(3),
+                                        Padding = new MarginPadding(4),
+                                        Spacing = new Vector2(5, 0),
                                         Children = new Drawable[]
                                         {
+                                            _icon = new Sprite
+                                            {
+                                                Size = new Vector2(18),
+                                                FillMode = FillMode.Fit
+                                            },
                                             new SpriteText
                                             {
                                                 Text = Name,
@@ -182,7 +191,8 @@ namespace osu.Framework.Design.Designer
                                 Padding = new MarginPadding
                                 {
                                     Left = 18
-                                }
+                                },
+                                Alpha = 0
                             }
                         }
                     }
@@ -191,11 +201,77 @@ namespace osu.Framework.Design.Designer
 
             Bindable<Document> _document { get; set; }
 
+            Texture _tex;
+            Texture _texOpen;
+
             [BackgroundDependencyLoader]
-            void load(Workspace workspace)
+            void load(Workspace workspace, TextureStore textures)
             {
                 _document = workspace.CurrentDocument.GetBoundCopy();
                 _document.BindValueChanged(d => Highlight(_doc != null && d == _doc), true);
+
+                string tex;
+
+                if (_doc == null)
+                {
+                    switch (Name.ToLowerInvariant())
+                    {
+                        case "bin": tex = "folder_type_binary"; break;
+                        case "src": tex = "folder_type_src"; break;
+                        case "temp":
+                        case ".temp":
+                        case "tmp":
+                        case ".tmp": tex = "folder_type_temp"; break;
+                        case "test":
+                        case "tests": tex = "folder_type_test"; break;
+                        case "tools":
+                        case ".tools": tex = "folder_type_tools"; break;
+                        case ".vscode": tex = "folder_type_vscode"; break;
+                        default: tex = "default_folder"; break;
+                    }
+                }
+                else
+                {
+                    switch (_doc.File.Name.ToLowerInvariant())
+                    {
+                        case "license": tex = "file_type_license"; break;
+                        case "launch.json": tex = "file_type_vscode"; break;
+                        case "tasks.json": tex = "file_type_vscode"; break;
+                        case "settings.json": tex = "file_type_vscode"; break;
+                    }
+                    switch (_doc.File.Extension.ToLowerInvariant())
+                    {
+                        case ".mp3":
+                        case ".ogg": tex = "file_type_audio"; break;
+                        case ".a":
+                        case ".bin":
+                        case ".exe":
+                        case ".dll": tex = "file_type_binary"; break;
+                        case ".config": tex = "file_type_config"; break;
+                        case ".cs": tex = "file_type_csharp"; break;
+                        case ".csproj": tex = "file_type_csproj"; break;
+                        case ".diff": tex = "file_type_diff"; break;
+                        case ".gitignore":
+                        case ".gitattributes": tex = "file_type_git"; break;
+                        case ".json": tex = "file_type_json"; break;
+                        case ".md":
+                        case ".markdown": tex = "file_type_markdown"; break;
+                        case ".text":
+                        case ".txt": tex = "file_type_text"; break;
+                        case ".vb": tex = "file_type_vb"; break;
+                        case ".vbproj": tex = "file_type_vbproj"; break;
+                        case ".avi":
+                        case ".mp4": tex = "file_type_video"; break;
+                        case ".xoml": tex = "file_type_view"; break;
+                        case ".xml": tex = "file_type_xml"; break;
+                        default: tex = "default_file"; break;
+                    }
+                }
+
+                _tex = textures.Get(tex);
+                _texOpen = textures.Get($"{tex}_opened");
+
+                _icon.Texture = _tex;
             }
 
             protected override bool OnHover(HoverEvent e)
@@ -219,7 +295,7 @@ namespace osu.Framework.Design.Designer
                     _hierarchyMarker.FadeOut(200);
             }
 
-            bool _collapsed;
+            bool _collapsed = true;
 
             protected override bool OnClick(ClickEvent e)
             {
@@ -228,9 +304,15 @@ namespace osu.Framework.Design.Designer
                 if (_doc == null)
                 {
                     if (_collapsed)
+                    {
                         _content.FadeIn(200);
+                        _icon.Texture = _texOpen ?? _tex;
+                    }
                     else
+                    {
                         _content.FadeOut(30);
+                        _icon.Texture = _tex;
+                    }
 
                     _collapsed = !_collapsed;
                 }
