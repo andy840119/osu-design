26a39e0e9a9d9d7c6f9ac2dfed7c02c2a733a899
phosphene47 <phosphene47@gmail.com>
2019-01-23 01:25:19 +0900

    Improve default editor styling

--- osu.Framework.Design/CodeEditor/HighlightStyleCollection.cs
+++ osu.Framework.Design/CodeEditor/HighlightStyleCollection.cs
@@ -7,13 +7,13 @@ namespace osu.Framework.Design.CodeEditor
     {
         public static HighlightStyleCollection Default => new HighlightStyleCollection
         {
-            { HighlightType.String, new HighlightStyle(new Color4(119, 66, 25, 255), HighlightFont.Normal) },
-            { HighlightType.SinglelineComment, new HighlightStyle(new Color4(51, 183, 49, 255), HighlightFont.Normal) },
-            { HighlightType.MultilineComment, new HighlightStyle(new Color4(51, 183, 49, 255) , HighlightFont.Normal)},
-            { HighlightType.Number, new HighlightStyle(new Color4(180, 73, 204, 255), HighlightFont.Normal) },
-            { HighlightType.Attribute, new HighlightStyle(new Color4(83, 158, 132, 255), HighlightFont.Normal) },
-            { HighlightType.Keyword, new HighlightStyle(new Color4(91, 105, 216, 255), HighlightFont.Normal) },
-            { HighlightType.Entity, new HighlightStyle(new Color4(211, 209, 91, 255), HighlightFont.Bold) }
+            { HighlightType.String, new HighlightStyle(new Color4(209, 124, 58, 255), HighlightFont.Normal) },
+            { HighlightType.SinglelineComment, new HighlightStyle(new Color4(77, 112, 77, 255), HighlightFont.Italic) },
+            { HighlightType.MultilineComment, new HighlightStyle(new Color4(77, 112, 77, 255) , HighlightFont.Italic)},
+            { HighlightType.Number, new HighlightStyle(new Color4(211, 120, 232, 255), HighlightFont.Normal) },
+            { HighlightType.Attribute, new HighlightStyle(new Color4(106, 209, 173, 255), HighlightFont.Normal) },
+            { HighlightType.Keyword, new HighlightStyle(new Color4(110, 124, 229, 255), HighlightFont.Normal) },
+            { HighlightType.Entity, new HighlightStyle(new Color4(239, 237, 107, 255), HighlightFont.Bold) }
         };
     }
 }
